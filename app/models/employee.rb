249058f1988c5fbc2a455573f64d5dca6bd149ab
Masakazu_Harada <seungri.uhm@gmail.com>
2023-11-04 22:54:44 +0900

    topページの作成

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,7 @@ class Employee < ApplicationRecord
   belongs_to :branch
   belongs_to :department
   belongs_to :boss
+
+  enum admin: { general: 0, admin: 1 } #admin権限をenumで管理する　0:一般, 1:管理者
+  enum enable: { inactive: 0, active: 1 } #社員の退職状況をenumで管理する　0:無効（退職）, 1:有効（在籍中）
 end
