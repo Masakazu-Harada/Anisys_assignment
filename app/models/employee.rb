4c168556356cba3a46ec0d7fd266ab4b976da0cd
Masakazu_Harada <seungri.uhm@gmail.com>
2023-11-09 14:38:56 +0900

    employeeモデルにboss_idを追加して自己結合の処理を記述する

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,7 +8,9 @@ class Employee < ApplicationRecord
 
   belongs_to :branch, optional: true #optional: trueでnilを許可する
   belongs_to :department, optional: true  #optional: trueでnilを許可する
-  belongs_to :boss, optional: true #optional: trueでnilを許可する
+  
+  belongs_to :boss, class_name: 'Employee', foreign_key: 'boss_id', optional: true #上司の従業員情報を取得するためのアソシエーション
+  has_many :team_menbers, class_name: 'Employee', foreign_key: 'boss_id' #部下の従業員情報を取得するためのアソシエーション
 
   enum admin: { general: 0, admin: 1 } #admin権限をenumで管理する　0:一般, 1:管理者
   enum enable: { inactive: 0, active: 1 } #社員の退職状況をenumで管理する　0:無効（退職）, 1:有効（在籍中）
