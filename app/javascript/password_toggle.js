3b1a2075b67c60baa834e40d1a50a71a9c9655d9
Masakazu_Harada <seungri.uhm@gmail.com>
2023-11-20 12:48:34 +0900

    jsを使ってパスワードコラプスの際に他のフォームの入力を無効にする

--- app/javascript/password_toggle.js
+++ app/javascript/password_toggle.js
@@ -1,18 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
+function setupPasswordToggle() {
   // パスワードを更新するボタンを取得
   const updatePasswordButton = document.getElementById('update-password-button');
   // 他のフォームフィールドを取得
   const formFields = document.querySelectorAll('.form-control:not(#new-password-field, #new-password-confirmation-field)');
 
-  updatePasswordButton.addEventListener('click', function () {
-    // フォームフィールドを無効にする
-    formFields.forEach(field => field.disabled = true);
-  });
+  if (updatePasswordButton) {
+    updatePasswordButton.addEventListener('click', function () {
+      // フォームフィールドを無効にする
+      formFields.forEach(field => field.disabled = true);
+    });
+  }
 
   // コラプスが閉じるボタンを取得
   const closeCollapseButton = document.getElementById('close-collapse-button');
-  closeCollapseButton.addEventListener('click', function () {
-    // フォームフィールドを有効にする
-    formFields.forEach(field => field.disabled = false);
-  });
-});
+  if (closeCollapseButton) {
+    closeCollapseButton.addEventListener('click', function () {
+      // フォームフィールドを有効にする
+      formFields.forEach(field => field.disabled = false);
+    });
+  }
+}
+
+// ドキュメントが読み込まれたときに実行
+document.addEventListener('DOMContentLoaded', setupPasswordToggle);
+
+// Turboリンクのページ遷移後にも実行
+document.addEventListener('turbo:load', setupPasswordToggle);
